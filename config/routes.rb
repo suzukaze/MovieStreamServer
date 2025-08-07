3e116f494022fed8531db5470f75db95cdaa1bd6
Jun Hiroe <Jun.Hiroe@gmail.com>
2025-08-07 14:33:49 +0900

    Revert "動画をキーワードで検索するAPIを作成"
    This reverts commit cc04015ffd98cad4e0779bec3a8956c46040c540.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
       get "genres", to: "genres#index"
     end
   end
-  get "movies/search"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- test/controllers/movies_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class MoviesControllerTest < ActionDispatch::IntegrationTest
-  test "should get search" do
-    get movies_search_url
-    assert_response :success
-  end
-end
