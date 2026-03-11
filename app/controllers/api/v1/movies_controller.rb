9bfdf6ffd717d6abaacbba702b1023b4ef4e03fd
Jun Hiroe <Jun.Hiroe@gmail.com>
2026-03-11 10:56:56 +0900

    動画一覧を取得するAPIを追加

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::MoviesController < ApplicationController
+  def index
+    movies = Movie.all
+    limit = params[:limit].to_i
+    if limit > 0
+      movies = movies.limit(limit)
+    end
+
+    render json: {
+      movies: movies
+    }
+  end
+
   def search
     keyword = params[:keyword]
     movies = Movie.search_by_keyword(keyword)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get "movies", to: "movies#index"
       get "movies/search"
       get "genres", to: "genres#index"
     end
